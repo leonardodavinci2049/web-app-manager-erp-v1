b00956e12363f211d3e71861c320d59afa44c7a2
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2026-07-28 14:23:59 -0300

    chore: new service

---
+++ src/services/api-main/physical_product/index.ts
@@ -0,0 +1,44 @@
+export {
+  PhysicalProductServiceApi,
+  physicalProductServiceApi,
+} from "./physical-product-service-api";
+export type {
+  UIOrderItemCustomer,
+  UIPhysicalProduct,
+  UIPhysicalProductWarranty,
+} from "./transformers/transformers";
+export {
+  transformOrderItemCustomer,
+  transformOrderItemsCustomer,
+  transformPhysicalProduct,
+  transformPhysicalProducts,
+  transformPhysicalProductWarranties,
+  transformPhysicalProductWarranty,
+} from "./transformers/transformers";
+export type {
+  OrderItemCustomerEntity,
+  OrderItemFindAllCustomerRequest,
+  OrderItemFindAllCustomerResponse,
+  OrderItemFindIdCustomerRequest,
+  OrderItemFindIdCustomerResponse,
+  PhysicalProductEntity,
+  PhysicalProductFindAllRequest,
+  PhysicalProductFindAllResponse,
+  PhysicalProductWarrantyEntity,
+  PhysicalProductWarrantyIdCustomerRequest,
+  PhysicalProductWarrantyIdCustomerResponse,
+  PhysicalProductWarrantyIdRequest,
+  PhysicalProductWarrantyIdResponse,
+  PhysicalProductWarrantyMovCustomerRequest,
+  PhysicalProductWarrantyMovCustomerResponse,
+  PhysicalProductWarrantyMovRequest,
+  PhysicalProductWarrantyMovResponse,
+  PhysicalProductWarrantySearchRequest,
+  PhysicalProductWarrantySearchResponse,
+  PhysicalProductWarrantyWithProductEntity,
+} from "./types/physical-product-types";
+export {
+  PhysicalProductError,
+  PhysicalProductNotFoundError,
+  PhysicalProductValidationError,
+} from "./types/physical-product-types";
